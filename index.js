79a93069386fcd0c870ffd018b0aa556fe7c8876
Alex <alex@agencianuba.com>
2025-12-18 10:20:24 -0600

    -ajuste index.js

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 const server = http.createServer(app);
 const io = socketIo(server, {
